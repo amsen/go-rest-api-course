9e0656c3e0e71234eb1d527be7f043ee1e8f2627
Amit Sengupta <amit.ksengupta@gmail.com>
2022-06-11 21:13:28 +0200

    Added basic database connectivity code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/amsen/go-rest-api-course/internal/database"
+)
 
 // Run - is going to be responsible for
 // the instantiation and startup of our
 // go application
 func Run() error {
 	fmt.Println("starting up our application")
+
+	dbConn, err := database.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database.")
+		return err
+	}
+
+	if err := dbConn.Ping(context.Background()); err != nil {
+		fmt.Println("Failed to ping the database.")
+		return err
+	}
+
+	fmt.Println("Sucessfully pinged the database..")
+
 	return nil
 }
 
---
+++ internal/database/database.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" //allows us to use the postgre driver
+)
+
+type Database struct {
+	Client *sql.DB
+}
+
+func NewDatabase() (*Database, error) {
+
+	fmt.Println("Setting up a new database conneciton...")
+
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_TABLE"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("SSL_MODE"),
+	)
+
+	dbConn, err := sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		return &Database{}, fmt.Errorf("error connecting to the database.. %w", err)
+	}
+
+	return &Database{
+		Client: dbConn.DB,
+	}, nil
+}
+
+func (db *Database) Ping(ctx context.Context) error {
+	return db.Client.PingContext(ctx)
+}
